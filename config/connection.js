9d883d54d8d6256433ee5c5d71f437614c865144
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-07-25 20:22:18 -0500

    adding heroku connection

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,31 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 // create the connection information for the sql database
-const connection = mysql.createConnection({
-    host: "localhost",
-    // Your port; if not 3306
-    port: 3306,
-    // Your username
-    user: "root",
-    // Your password
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        // Your port; if not 3306
+        port: 3306,
+        // Your username
+        user: "root",
+        // Your password
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    });
+}
+
+
+connection.connect();
+
+
+// (function(err) {
+//     if (err) throw err;
+//     console.log("connected as id " + connection.threadId);
+// });
 
 // connection.query = util.promisify(connection.query)
 module.exports = connection
